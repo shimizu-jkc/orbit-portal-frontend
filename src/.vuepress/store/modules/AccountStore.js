7d227680b0c586b3ae6eda0497b01647f954b532
shimizu <58317533+shimizu-jkc@users.noreply.github.com>
2021-03-24 10:12:05 +0900

    テストデータの環境対応 (#103)
    * Add test data
    
    * Add test-deploy trigger
    
    * Add value to dummy env.json

--- src/.vuepress/store/modules/AccountStore.js
+++ src/.vuepress/store/modules/AccountStore.js
@@ -1,9 +1,10 @@
 import AccountApi from '../../api/AccountApi'
+import env from "../../env.json";
 
 // parameter schema
 const schema = {
   Env: "",
-  BillingAFFCode: "",
+  BillingAFFCode: (env.ENV === "dev") ? "1000.100600.0000.OT001.000.6510210.000000.0000000000.0.0" : "",
   Files: [],
   StartOperationDate: 0,
   ExpireOperationDate: 0,
